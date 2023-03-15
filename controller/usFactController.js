8b6617c37a4c2d8fa447938c55ea7db545514865
Alfredoeb9 <alfredoeb96@gmail.com>
2023-03-14 17:30:00 -0700

    [usFact] created routes to retrieve specific US Fact and get all US Fact data

--- controller/usFactController.js
+++ controller/usFactController.js
@@ -1,6 +1,42 @@
 const USFact = require("../models/usFactModel");
 const mongoose = require("mongoose");
 
+const getUSFact = async (req, res) => {
+  const { id } = req.params;
+  const { quizNum } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No Such Quiz!" });
+  }
+
+  const usFact = await USFact.findById(id);
+
+  // if (req.method == "GET") {
+  //   geolocationQuiz = await Geolocation.findById(id);
+  //   // return geolocationQuiz;
+  // } else if (req.method == "POST") {
+  //   geolocationQuiz = await Geolocation.findById(id, {
+  //     questions: { $slice: Number(quizNum) },
+  //   });
+  // return geolocationQuiz;
+  // }
+
+  if (!usFact) {
+    return res.status(400).json({ error: "No Such US Fact!" });
+  }
+
+  return res.status(200).json(usFact);
+};
+
+const getAllUSFacts = async (req, res) => {
+  try {
+    const allUSFacts = await USFact.find();
+    return res.status(200).json(allUSFacts);
+  } catch (error) {
+    return res.status(400).json({ error: error });
+  }
+};
+
 const getSpecificUSFact = async (req, res) => {
   try {
     const { usState } = req.body;
@@ -49,4 +85,6 @@ const createUSFact = async (req, res) => {
 module.exports = {
   createUSFact,
   getSpecificUSFact,
+  getAllUSFacts,
+  getUSFact,
 };
--- routes/usFactRoutes.js
+++ routes/usFactRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const {
   createUSFact,
   getSpecificUSFact,
+  getAllUSFacts,
+  getUSFact,
 } = require("../controller/usFactController");
 
 const { verifyAdmin } = require("../middleware/requireAuth");
+
+router.get("/", getAllUSFacts);
+router.get("/:id", getUSFact);
 
 router.post("/createUSFact", verifyAdmin, createUSFact);
 router.post("/getSpecificUSFact", getSpecificUSFact);

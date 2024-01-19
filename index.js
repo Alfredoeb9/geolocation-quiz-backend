ff71e0916f66a079349ad9342f303a2431533d50
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-19 15:10:03 -0800

    [test] test

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/authRoutes");
-const geolocationRoutes = require("./routes/geolocationRoutes");
-const userRoutes = require("./routes/userRoutes");
-const resultRoutes = require("./routes/resultRoutes");
-const usFactRoutes = require("./routes/usFactRoutes");
 
-// express app
-const app = express();
-
-// enable CORS
-app.use(cors());
-
-app.use(express.json());
-
-// middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.get("/", (req,res) => {
-  res.json("heelo")
-})
-
-app.use("/api/auth", authRoutes);
-app.use("/api/geolocation", geolocationRoutes);
-app.use("/api/usfact", usFactRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/result", resultRoutes);
-
-mongoose.set("strictQuery", false);
+const app = require("./api/index")
 
 mongoose
   .connect(process.env.MONGO_URI, {
